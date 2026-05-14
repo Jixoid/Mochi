505daa7c0d1a03475e88115cd0ff09114846a6a9
Kadir Aydın <jixoid@gmail.com>
2026-05-14 22:38:36 +0300

    fixed: resource embedding on windows

--- mochi/vfs/vfs_res.cc
+++ mochi/vfs/vfs_res.cc
@@ -15,10 +15,18 @@
 #include <istream>
 #include <string>
 #include <string_view>
-#include <dlfcn.h>
 #include <spanstream>
 #include "mochi/basis.hh"
 #include "mochi/vfs/vfs.hh"
+
+#if defined(_WIN32)
+  #ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+#else
+  #include <dlfcn.h>
+#endif
 
 
 
@@ -38,9 +46,17 @@ namespace vfs::__res
 					if (C == '.' | C == '/' | C == '-')
 						C = '_';
 
+				void *Sym_Beg{}, *Sym_End{};
 
-				auto Sym_Beg = dlsym(RTLD_DEFAULT, ("_binary_res_"+fpath+"_start").c_str());
-				auto Sym_End = dlsym(RTLD_DEFAULT, ("_binary_res_"+fpath+"_end").c_str());
+				// Platforma özgü sembol yükleme
+				#if defined(_WIN32)
+						HMODULE hMod = GetModuleHandle(NULL);
+					Sym_Beg = (void*)GetProcAddress(hMod, ("_binary_res_"+fpath+"_start").c_str());
+					Sym_End = (void*)GetProcAddress(hMod, ("_binary_res_"+fpath+"_end").c_str());
+				#else
+					Sym_Beg = dlsym(RTLD_DEFAULT, ("_binary_res_"+fpath+"_start").c_str());
+					Sym_End = dlsym(RTLD_DEFAULT, ("_binary_res_"+fpath+"_end").c_str());
+				#endif
 
 				if (!Sym_Beg || !Sym_End)
 					throw std::filesystem::filesystem_error("The embedded file could not be opened.", _fpath, std::make_error_code(std::errc::no_such_file_or_directory));
